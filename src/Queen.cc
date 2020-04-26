0a65af7ca91a5b85008e5619435234d9e650c04d
tomokos2 <tomokos2@illinois.edu>
2020-04-26 10:25:37 -0500

    Implemented queen movement

--- src/Queen.cc
+++ src/Queen.cc
@@ -2,6 +2,8 @@
 // Created by tomok on 4/19/2020.
 //
 
+#include <chess/PieceClasses/Rook.h>
+#include <chess/PieceClasses/Bishop.h>
 #include "chess/PieceClasses/Queen.h"
 
 namespace chess {
@@ -10,13 +12,20 @@ namespace chess {
       is_pawn_{is_pawn} {}
 
   bool Queen::IsLegalMove(std::pair<std::pair<int,int>,std::pair<int,int>> turn) {
-    return false;
+    Rook rook(is_white_, false);
+    Bishop bishop(is_white_, false);
+    return rook.IsLegalMove(turn) || bishop.IsLegalMove(turn);
   }
 
   // TODO: Make a rook or a bishop and return their path.
   std::vector<std::pair<int,int>> Queen::GetPath(std::pair<std::pair<int,int>,std::pair<int,int>> turn) {
-    std::vector<std::pair<int,int>> path;
-    return path;
+    Rook rook(is_white_, false);
+    Bishop bishop(is_white_, false);
+    if (rook.IsLegalMove(turn)) {
+      return rook.GetPath(turn);
+    } else {
+      return bishop.GetPath(turn);
+    }
   }
 
 }
