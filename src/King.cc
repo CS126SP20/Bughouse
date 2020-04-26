199df44a204b0f00384defcc7a76cf72c6676ab4
tomokos2 <tomokos2@illinois.edu>
2020-04-26 10:35:31 -0500

    Implemented knight and king movement

--- src/King.cc
+++ src/King.cc
@@ -8,7 +8,8 @@ namespace chess {
   King::King(bool is_white) : is_white_(is_white) {}
 
   bool King::IsLegalMove(std::pair<std::pair<int,int>,std::pair<int,int>> turn) {
-    return false;
+    return abs(turn.first.first - turn.second.first) <= 1 
+          && abs(turn.first.second - turn.second.second) <= 1;
   }
 
   std::vector<std::pair<int,int>> King::GetPath(std::pair<std::pair<int,int>,std::pair<int,int>> turn) {
--- src/Knight.cc
+++ src/Knight.cc
@@ -2,6 +2,7 @@
 // Created by tomok on 4/19/2020.
 //
 
+#include <chess/BoardEngine.h>
 #include "chess/PieceClasses/Knight.h"
 
 namespace chess {
@@ -11,7 +12,10 @@ namespace chess {
       is_pawn_{is_pawn} {}
 
   bool Knight::IsLegalMove(std::pair<std::pair<int,int>,std::pair<int,int>> turn) {
-    return false;
+    int row_diff = abs(turn.first.first - turn.second.first);
+    int col_diff = abs(turn.first.second - turn.second.second);
+    return ((turn.first.second == EMPTY)) ||
+          (row_diff == 2 && col_diff == 1) || (row_diff == 1 && col_diff == 2);
   }
   std::vector<std::pair<int,int>> Knight::GetPath(std::pair<std::pair<int,int>,std::pair<int,int>> turn) {
     std::vector<std::pair<int,int>> path;
