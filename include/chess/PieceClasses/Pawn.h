a62b24772aef9bf14f24babd2cb5477340860635
tomokos2 <tomokos2@illinois.edu>
2020-05-05 15:37:27 -0500

    Documented Pawn.h and Pawn.cc

--- include/chess/PieceClasses/Pawn.h
+++ include/chess/PieceClasses/Pawn.h
@@ -9,26 +9,32 @@
 
 namespace chess {
   
- class Pawn : public Piece {
-   bool is_white_;
-   bool is_poss_double_move_;
-   bool can_double_move_;
-   //TODO: A PAWN CAN BE ANY PIECE!
+class Pawn : public Piece {
+
  public:
-   explicit Pawn(bool is_white);
+  explicit Pawn(bool is_white);
 
-   bool GetIsWhite() override { return is_white_; }
+  bool GetIsWhite() override { return is_white_; }
    
-   PieceType GetPieceType() override { return PAWN; };
+  PieceType GetPieceType() override { return PAWN; };
 
-   bool IsPawn() override { return true; }
+  bool IsPawn() override { return true; }
 
-   bool Pawn::IsLegalMove(std::pair<Location, Location> turn) override;
+  bool Pawn::IsLegalMove(std::pair<Location, Location> turn) override;
    
-   std::vector<Location> GetPath(std::pair<Location, Location> turn) override;
+  std::vector<Location> GetPath(std::pair<Location, Location> turn) override;
 
-   void DoTurn() override;
- };
+  void DoTurn() override;
+  
+ private:
+  bool is_white_;
+  // The pawn may double move -- depending on whether the board validates that it can go there
+  bool is_poss_double_move_;
+  bool can_double_move_;
+  
+  // The pawn is placed from the hand
+  bool IsValidPlacement(Location pawn_destination);
+};
  
 }
 #endif //FINALPROJECT_PAWN_H
--- src/Pawn.cc
+++ src/Pawn.cc
@@ -13,25 +13,21 @@ Pawn::Pawn(bool is_white)
   { }
 
 bool Pawn::IsLegalMove(std::pair<Location, Location> turn) {
+  
   if (turn.first.Col() == EMPTY) {
-    if (is_white_) {
-      if (turn.second.Row() == 0) {
-        return false;
-      }
-    } else {
-      if (turn.second.Row() == kBoardSize - 1) {
-        return false;
-      }
-    }
-    return true;
+    return IsValidPlacement(turn.second);
   }
+  
   
   bool is_valid = false;
   if (turn.first.Col() == turn.second.Col()) {
+    
     if (is_white_) {
       if (turn.first.Row() == turn.second.Row() + 1) {
+        // Pawn can move one space forward
         is_valid = true;
       } else if (can_double_move_ && turn.first.Row() == turn.second.Row() + 2) {
+        // Pawn can move two spaces forward if it is first move
         is_valid = true;
       }
     } else {
@@ -43,7 +39,7 @@ bool Pawn::IsLegalMove(std::pair<Location, Location> turn) {
     }
   }
   
-  
+  // Pawn can move diagonally one space forward
   if (abs(turn.first.Col() - turn.second.Col()) == 1) {
     if (is_white_) {
       if (turn.first.Row() == turn.second.Row() + 1) {
@@ -56,7 +52,6 @@ bool Pawn::IsLegalMove(std::pair<Location, Location> turn) {
     }
   }
   
-  
   return is_valid;
 }
 
@@ -64,33 +59,65 @@ std::vector<Location> Pawn::GetPath(std::pair<Location, Location> turn) {
   std::vector<Location> path;
   
   if (turn.first.Col() == EMPTY) {
+    // The pawn needs to be placed on an empty square
     path.push_back(turn.second);
+    
+    // If the pawn is placed from the hand, it can no longer double move
     can_double_move_ = false;
     return path;
   }
   
+  // The pawn moved diagonally, so it must be a capture (en passant or normal)
   if (abs(turn.first.Col() - turn.second.Col()) == 1) {
+    // Empty path for capture
     return path;
   }
   
+  // Double move checks
   if (turn.first.Row() == turn.second.Row() + 2) {
+    
+    // Point in front of pawn must be empty
     auto point = Location(turn.second.Row() + 1, turn.second.Col());
     path.push_back(point);
     is_poss_double_move_ = true;
+    
   } else if (turn.first.Row() == turn.second.Row() - 2) {
+    
+    // Point in front of pawn must be empty
     auto point = Location(turn.second.Row() - 1, turn.second.Col());
     path.push_back(point);
     is_poss_double_move_ = true;
+    
   } else {
+    // Not a double move
     is_poss_double_move_ = false;
   }
+  
+  // The pawn must move to an empty square if it moves forward
   path.push_back(turn.second);
   
   return path;
 }
 
 void Pawn::DoTurn() {
+  // Called after the pawn actually moves.
+  // Turn off double moving, since the pawn moved
   can_double_move_ = false;
   is_poss_double_move_ = false;
 }
+
+bool Pawn::IsValidPlacement(Location pawn_destination) {
+  // The pawn cannot be placed on the last rank
+  if (is_white_) {
+    if (pawn_destination.Row() == 0) {
+      return false;
+    }
+  } else {
+    if (pawn_destination.Row() == kBoardSize - 1) {
+      return false;
+    }
+  }
+  return true;
+}
+
 }  
