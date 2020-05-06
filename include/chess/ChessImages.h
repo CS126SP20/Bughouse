23c31f02303cad8805a359ceaa66b993a3bb8482
tomokos2 <tomokos2@illinois.edu>
2020-05-05 23:07:53 -0500

    Documented chess images

--- include/chess/ChessImages.h
+++ include/chess/ChessImages.h
@@ -23,26 +23,30 @@ const auto kGreyCol = ci::Color(1.0f * 91/255, 1.0f * 89/255, 1.0f * 89/255);
 const auto kBlackCol = ci::Color(1.0f * 62/255, 1.0f * 54/255, 1.0f * 52/255);
 const auto kWhiteCol = ci::Color(1.0f * 248 /255, 1.0f * 244/255, 1.0f * 236/255);
 
-
 class ChessImages {
-  public:
-    void SetUp();
-    cinder::gl::Texture2dRef &RetrievePieceImage(PieceType piece_type, bool is_white);
-    cinder::gl::Texture2dRef &RetrieveBoardImage() { return board_img_;};
-  private:
-    cinder::gl::Texture2dRef board_img_;
-    cinder::gl::Texture2dRef bb_img_;
-    cinder::gl::Texture2dRef bw_img_;
-    cinder::gl::Texture2dRef kb_img_;
-    cinder::gl::Texture2dRef kw_img_;
-    cinder::gl::Texture2dRef nb_img_;
-    cinder::gl::Texture2dRef nw_img_;
-    cinder::gl::Texture2dRef pb_img_;
-    cinder::gl::Texture2dRef pw_img_;
-    cinder::gl::Texture2dRef qb_img_;
-    cinder::gl::Texture2dRef qw_img_;
-    cinder::gl::Texture2dRef rb_img_;
-    cinder::gl::Texture2dRef rw_img_;
-  };
+ public:
+  // Loads all of the images
+  void SetUp();
+  
+  // Returns the image of a piece
+  cinder::gl::Texture2dRef &RetrievePieceImage(PieceType piece_type, bool is_white);
+  
+  // Returns the board image
+  cinder::gl::Texture2dRef &RetrieveBoardImage() { return board_img_;};
+ private:
+  cinder::gl::Texture2dRef board_img_; // Board
+  cinder::gl::Texture2dRef bb_img_;    // Bishop, black
+  cinder::gl::Texture2dRef bw_img_;    // Bishop, white
+  cinder::gl::Texture2dRef kb_img_;    // King,   black
+  cinder::gl::Texture2dRef kw_img_;    // King,   white
+  cinder::gl::Texture2dRef nb_img_;    // Knight, black
+  cinder::gl::Texture2dRef nw_img_;    // Knight, white
+  cinder::gl::Texture2dRef pb_img_;    // Pawn,   black
+  cinder::gl::Texture2dRef pw_img_;    // Pawn,   white
+  cinder::gl::Texture2dRef qb_img_;    // Queen,  black
+  cinder::gl::Texture2dRef qw_img_;    // Queen,  white
+  cinder::gl::Texture2dRef rb_img_;    // Rook,   black
+  cinder::gl::Texture2dRef rw_img_;    // Rook,   white
+};
 }
 #endif //FINALPROJECT_CHESSIMAGES_H
