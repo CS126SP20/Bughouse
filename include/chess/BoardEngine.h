5881ae7092f21ed20952432afe253d4519eab434
tomokos2 <tomokos2@illinois.edu>
2020-05-05 22:18:35 -0500

    Documented BoardEngine.cc, documented BoardEngine.h

--- include/chess/BoardEngine.h
+++ include/chess/BoardEngine.h
@@ -23,6 +23,7 @@ namespace chess {
 class BoardEngine {
   
 public:
+  
   // Default Constructor
   BoardEngine() { };
   
@@ -36,22 +37,30 @@ public:
   BoardEngine(ChessImages *chess_images, ci::Area board_bounds, ci::Area top_box_bounds, ci::Area bottom_box_bounds,
               Player team1_player, Player team2_player);
 
-  // Called on click, updates turn_ according to the user action.
-  void ProcessClick(ci::vec2 point);
-
-  void Draw();
-  void ReceivePiece(Piece* piece);
-  Piece* Move();
-  bool IsGameEnd() { return current_game_state_ == GameState::kTeam1Win 
-                            || current_game_state_ == GameState::kTeam2Win; };
-  ci::Color GetWinnerColor();
+  // State of game. Starts as kPlaying
   GameState current_game_state_;
 
-  
   // Dynamically changed every time the board flips for the other player
   bool is_white_turn_;
   
+  // Called on click, updates turn_ according to the user action.
+  void ProcessClick(ci::vec2 point);
 
+  // Called by update, moves board if turn is full
+  Piece* Move();
+  
+  // Wrapper for the board object to receive a piece from the other engine
+  void ReceivePiece(Piece* piece);
+  
+  // Check to make sure the game is still going
+  bool IsGameEnd() { return current_game_state_ == GameState::kTeam1Win 
+                            || current_game_state_ == GameState::kTeam2Win; };
+  
+  // Called at the end of the round
+  ci::Color GetWinnerColor();
+
+  // Called by cinder draw. Draws board view.
+  void Draw();
   
 private:
   // A board object to represent the board
@@ -63,14 +72,26 @@ private:
   // Represents a move of a piece from one point to another.
   // Updated every time the user clicks the screen
   std::pair<Location, Location> turn_;
+  
+  // The last move that the player made
   std::pair<Location, Location> last_turn_;
+  
+  // Players know their team, their time, and their color
   Player team_1_player_;
   Player team_2_player_;
+  
+  // Called by Move, changes game state depending on the board move
   GameState UpdateGameState(Piece* captured);
+  
+  // Called by process click
+  // Updates the turn with the click location
   void UpdateTurnWithBoardClick(Location& click);
   void UpdateTurnWithBoxClick(Location& click);
 
+  // Called during Move, to make sure the game hasn't ended
   void CheckTimeOut();
+  
+  // Called by move, swaps current players and changes timers
   void SwapTurns();
   
 };
--- src/BoardEngine.cc
+++ src/BoardEngine.cc
@@ -22,19 +22,19 @@ BoardEngine::BoardEngine(ChessImages *chess_images, ci::Area board_bounds, ci::A
     {
 }
 
-void BoardEngine::CheckTimeOut() {
-  if (team_2_player_.timer_.getSeconds() >= kMaxSeconds) {
-    current_game_state_ = GameState::kTeam1Win;
-  } else if (team_1_player_.timer_.getSeconds() >= kMaxSeconds) {
-    current_game_state_ = GameState::kTeam2Win;
-  }
-}
-
+//================================================================
+// Public member functions
+//================================================================
 
 void BoardEngine::ProcessClick(ci::vec2 point) {
+  
   if (current_game_state_ == GameState::kPawnPromotion) {
+    // The click has to be a promotion choice    
     PieceType choice = board_view_.GetPieceTypeFromClick(point);
+
+    // Pawn is default if none is chosen
     if (choice != PAWN) {
+      // Only promote and swap turns once the valid choice is made
       board_.PromotePawn(choice, is_white_turn_);
       current_game_state_ = GameState::kPlaying;
       SwapTurns();
@@ -42,106 +42,53 @@ void BoardEngine::ProcessClick(ci::vec2 point) {
     return;
   }
   
+  // Normal click, get the location from board view
   Location click = board_view_.ProcessClick(point);
   if (!click.IsEmpty()) {
     if (click.Col() == EMPTY) {
+      // They clicked the box
       UpdateTurnWithBoxClick(click);
     } else {
+      // They clicked the board
       UpdateTurnWithBoardClick(click);
     }
   }
 }
 
-void BoardEngine::UpdateTurnWithBoardClick(Location& click) {
-  Piece* piece = board_.GetPieceAtSquare(click);
-  
-  if (piece != nullptr && (piece->GetIsWhite() == is_white_turn_)) {
-    if (turn_.first == click) {
-      turn_.first.Empty();
-    } else {
-      turn_.first = click;
-    }
-  } else if (!turn_.first.IsEmpty() && turn_.second.IsEmpty()
-            && (piece == nullptr || piece->GetIsWhite() != is_white_turn_)) {
-    if (board_.IsValidMove(std::make_pair(turn_.first, click), is_white_turn_)) {
-      turn_.second = click;
-    } else {
-      turn_.first.Empty();
-    }
-    
+Piece* BoardEngine::Move() {
+  if (current_game_state_ == GameState::kPawnPromotion) {
+    // Don't do anything
+    // Waiting on the player to choose a promotion
+    return nullptr;
   }
   
-}
-
-void BoardEngine::UpdateTurnWithBoxClick(Location& click) {
-  if (click.Row() < 0 || click.Col() >= board_.GetHandSize(is_white_turn_)) return;
-  turn_.first = click;
+  Piece* captured = nullptr;
+  
+  if (!turn_.first.IsEmpty() && !turn_.second.IsEmpty()) {
+    // A full, valid turn has been made.
+    // Update the board
+    captured = board_.Update(turn_, is_white_turn_);
+    
+    // Update the state of the game
+    current_game_state_ = UpdateGameState(captured);
+    
+    // Only swap turns if the game is still playing (don't swap yet if pawn promotion)
+    if (current_game_state_ == GameState::kPlaying) {
+      SwapTurns();
+    }
+  }
+  
+  // Make sure nobody ran out of time yet
+  CheckTimeOut();
+  
+  // Give the captured piece to the other engine
+  return captured;
 }
 
 void BoardEngine::ReceivePiece(Piece* piece) {
   if (piece == nullptr) return;
   board_.ReceivePiece(piece);
 }
-
-Piece* BoardEngine::Move() {
-  if (current_game_state_ == GameState::kPawnPromotion) {
-    return nullptr;
-  }
-  Piece* captured = nullptr;
-  
-  if (!turn_.first.IsEmpty() && !turn_.second.IsEmpty()) {
-    
-    captured = board_.Update(turn_, is_white_turn_);
-    
-    current_game_state_ = UpdateGameState(captured);
-    if (current_game_state_ == GameState::kPlaying) {
-      SwapTurns();
-    }
-  }
-  
-  CheckTimeOut();
-  return captured;
-}
-
-void BoardEngine::SwapTurns() {
-  last_turn_ = turn_;
-  turn_.first.Empty();
-  turn_.second.Empty();
-  is_white_turn_ = !is_white_turn_;
-  board_view_.SwapCurrentPlayerColor();
-  if (team_1_player_.is_white_ == is_white_turn_) {
-    team_1_player_.timer_.resume();
-    team_2_player_.timer_.stop();
-  } else {
-    team_1_player_.timer_.stop();
-    team_2_player_.timer_.resume();
-  }
-}
-
-GameState BoardEngine::UpdateGameState(Piece* captured) {
-  GameState current_state;
-  if (captured != nullptr && captured->GetPieceType() == KING) {
-    if (team_1_player_.is_white_ != captured->GetIsWhite()) {
-      current_state = GameState::kTeam1Win;
-    } else {
-      current_state = GameState::kTeam2Win;
-    }
-  } else if (board_.HasPromotedPawn(is_white_turn_)) {
-    current_state = GameState::kPawnPromotion;
-  } else {
-    current_state = GameState::kPlaying;
-  }
-  
-  return current_state;
-}
-
-void BoardEngine::Draw() {
-  board_view_.Draw(board_, team_1_player_, team_2_player_, turn_, last_turn_);
-  if (current_game_state_ == GameState::kPawnPromotion) {
-    board_view_.DrawPawnPromotion();
-  }
-}
-
 
 ci::Color BoardEngine::GetWinnerColor() {
   if (current_game_state_ == GameState::kTeam1Win) {
@@ -152,5 +99,104 @@ ci::Color BoardEngine::GetWinnerColor() {
   return ci::Color(0,0,0);
 }
 
+void BoardEngine::Draw() {
+  board_view_.Draw(board_, team_1_player_, team_2_player_, turn_, last_turn_);
+  
+  if (current_game_state_ == GameState::kPawnPromotion) {
+    board_view_.DrawPawnPromotion();
+  }
+}
+
+//================================================================
+// Private member functions
+//================================================================
+
+GameState BoardEngine::UpdateGameState(Piece* captured) {
+  GameState current_state;
+  
+  if (captured != nullptr && captured->GetPieceType() == KING) {
+    // The opponent king was captured! Game over
+    if (team_1_player_.is_white_ != captured->GetIsWhite()) {
+      current_state = GameState::kTeam1Win;
+    } else {
+      current_state = GameState::kTeam2Win;
+    }
+    
+  } else if (board_.HasPromotedPawn(is_white_turn_)) {
+    // The player promoted a pawn
+    current_state = GameState::kPawnPromotion;
+    
+  } else {
+    // Nothing special happened 
+    current_state = GameState::kPlaying;
+  }
+  
+  return current_state;
+}
+
+void BoardEngine::UpdateTurnWithBoardClick(Location& click) {
+  Piece* piece = board_.GetPieceAtSquare(click);
+  
+  if (piece != nullptr && (piece->GetIsWhite() == is_white_turn_)) {
+    // Player chose a piece of their own color
+    
+    if (turn_.first == click) {
+      // They un-clicked the previously piece
+      turn_.first.Empty();
+    } else {
+      // They clicked a new piece
+      turn_.first = click;
+    }
+  } else if (!turn_.first.IsEmpty() && turn_.second.IsEmpty()
+            && (piece == nullptr || piece->GetIsWhite() != is_white_turn_)) {
+    // They already chose a piece for the first move
+    // The second piece they chose is either an empty square or a piece of opponent color
+    
+    if (board_.IsValidMove(std::make_pair(turn_.first, click), is_white_turn_)) {
+      // If the move is valid, make it the second click
+      turn_.second = click;
+    } else {
+      // If the move is invalid, un-click the first box
+      turn_.first.Empty();
+    }
+  }
+}
+
+void BoardEngine::UpdateTurnWithBoxClick(Location& click) {
+  // Make sure it is a valid index choice
+  if (click.Row() < 0 || click.Col() >= board_.GetHandSize(is_white_turn_)) return;
+  
+  // If valid, make it the first click
+  turn_.first = click;
+}
+
+void BoardEngine::CheckTimeOut() {
+  if (team_2_player_.timer_.getSeconds() >= kMaxSeconds) {
+    current_game_state_ = GameState::kTeam1Win;
+  } else if (team_1_player_.timer_.getSeconds() >= kMaxSeconds) {
+    current_game_state_ = GameState::kTeam2Win;
+  }
+}
+
+void BoardEngine::SwapTurns() {
+  // Update the last turn
+  last_turn_ = turn_;
+  // Empty the current turn
+  turn_.first.Empty();
+  turn_.second.Empty();
+  
+  // Swap colors
+  is_white_turn_ = !is_white_turn_;
+  board_view_.SwapCurrentPlayerColor();
+  
+  // Stop the current timer, start the opponent timer
+  if (team_1_player_.is_white_ == is_white_turn_) {
+    team_1_player_.timer_.resume();
+    team_2_player_.timer_.stop();
+  } else {
+    team_1_player_.timer_.stop();
+    team_2_player_.timer_.resume();
+  }
+}
 
 }
